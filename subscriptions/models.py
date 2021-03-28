5e763387da6038a153bdfc30b4f06ef40581cc5d
Alexander Forrest <asforrest@icloud.com>
2021-03-28 01:12:46 +0000

    added subscriptions app and added models for subscription and categories.

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    # null=True and blank=True allows the field to be optional
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Subscription(models.Model):
+    # on_delete=models.SET_NULL allows a category to be deleted without it deleting the product
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    discounts = models.BooleanField(default=False, null=False, blank=False)
+    database_access = models.BooleanField(default=False)
+    forum_access = models.BooleanField(default=False)
+    email_support = models.BooleanField(default=False)
+    chat_support = models.BooleanField(default=False)
+    video_guides = models.BooleanField(default=False)
+    video_conferencing = models.BooleanField(default=False)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                 blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
