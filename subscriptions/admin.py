de3efd64fbe40d5f286e959ead1464186570c37a
Alexander Forrest <asforrest@icloud.com>
2021-03-28 17:32:16 +0000

    Customised admin view of subscriptions and categories

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -2,5 +2,34 @@ from django.contrib import admin
 from .models import Subscription, Category
 
 # Register your models here.
-admin.site.register(Subscription)
-admin.site.register(Category)
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'discounts',
+        'database_access',
+        'forum_access',
+        'email_support',
+        'chat_support',
+        'video_guides',
+        'video_conferencing',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Subscription, SubscriptionAdmin)
+admin.site.register(Category, CategoryAdmin)
