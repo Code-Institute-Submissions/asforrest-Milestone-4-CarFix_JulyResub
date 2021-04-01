70c08f3037f980b01af7b94816efe6bd91507da8
Alexander Forrest <asforrest@icloud.com>
2021-04-01 13:16:51 +0000

    Fixed the bug with the routing issue. Was a simple punctuation mistake.

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_subscriptions, name='subscriptions'),
-    path('subscription_detail', views.subscription_detail, name='subscription_detail'),
+    path('<subscription_id>', views.subscription_detail, name='subscription_detail'),
 ]
