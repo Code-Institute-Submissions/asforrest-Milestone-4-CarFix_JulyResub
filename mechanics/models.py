8816ab53ea41ed297773125e156c08b78373c419
Alexander Forrest <asforrest@icloud.com>
2021-04-04 23:38:00 +0000

    Added data for mechanics models

--- mechanics/models.py
+++ mechanics/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 
 class Brand(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Brands'
+
     name = models.CharField(max_length=254)
     # null=True and blank=True allows the field to be optional
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
@@ -19,13 +23,13 @@ class Mechanic(models.Model):
     # on_delete=models.SET_NULL allows a brand to be deleted without it deleting the mechanic
     brand = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.SET_NULL)
     mechanic_name = models.CharField(max_length=254)
-    year_joined = models.CharField(max_length=4, null=True, blank=True)
     primary_car_brand = models.CharField(max_length=254)
     biography = models.TextField(null=True, blank=True)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
-                                 blank=True)
+    year_joined = models.CharField(max_length=4, null=True, blank=True)
+    rating = models.DecimalField(max_digits=5, decimal_places=1, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
