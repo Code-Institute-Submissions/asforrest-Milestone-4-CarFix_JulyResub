4f6766d5c11e03f4a68a63f42a1826d905badaf3
Alexander Forrest <asforrest@icloud.com>
2021-04-05 00:14:50 +0000

    Customised Admin for mechanics models

--- mechanics/admin.py
+++ mechanics/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from .models import Mechanic, Brand
 
 # Register your models here.
-admin.site.register(Mechanic)
-admin.site.register(Brand)
+
+
+class MechanicAdmin(admin.ModelAdmin):
+    list_display = (
+        'mechanic_name',
+        'primary_car_brand',
+        'rating',
+        'biography',
+        'year_joined',
+        'image',
+    )
+
+    ordering = ('-rating',)
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = (
+            'name',
+            'friendly_name',
+        )
+
+
+admin.site.register(Mechanic, MechanicAdmin)
+admin.site.register(Brand, BrandAdmin)
