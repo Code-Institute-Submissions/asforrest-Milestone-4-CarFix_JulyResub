e22905a08657d1702de7b9720a630f6e3150341f
Alexander Forrest <asforrest@icloud.com>
2021-04-05 23:05:35 +0000

    Trying to arrange filters

--- mechanics/views.py
+++ mechanics/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Mechanic
+from .models import Mechanic, Brand
 
 
 def all_mechanics(request):
@@ -9,12 +9,21 @@ def all_mechanics(request):
 
     mechanics = Mechanic.objects.all()
     query = None
+    brands = None
+
+# This is from the products filtering and searching lessons and is NOT WORKING correctly, this may be due to the disambiguation between primary_car_brand and brand which
+# was used as the foreign key identifier. Although the Botique Ado info is set up in the same manner.
 
     if request.GET:
+        if 'brand' in request.GET:
+            brands = request.GET[brand].split[',']
+            mechanics = mechanics.filter(brand__name__in=brands)
+            brands = Brand.objects.filter(name__in=brands)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria!")
+                messages.error(request, "You didn't enter any search criteria")
                 return redirect(reverse('products'))
 
             queries = Q(mechanic_name__icontains=query) | Q(primary_car_brand__icontains=query)
@@ -23,6 +32,7 @@ def all_mechanics(request):
     context = {
         'mechanics': mechanics,
         'search_term': query,
+        'current_brands': brands,
     }
 
     return render(request, 'mechanics/mechanics.html', context)
