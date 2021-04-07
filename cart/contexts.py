1d750e7a3e9df4445816e673a15f5a84da9089f3
Alexander Forrest <asforrest@icloud.com>
2021-04-07 17:06:47 +0000

    Item cost is being added to shopping cart in header

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,14 +1,27 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from subscriptions.models import Subscription
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
-    product_count = 0
+    subscription_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        subscription = get_object_or_404(Subscription, pk=item_id)
+        total += quantity * subscription.price
+        subscription_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'subscription': subscription,
+        })
 
     context = {
         'cart_items': cart_items,
         'total': total,
-        'product_count': product_count,
+        'subscription_count': subscription_count,
     }
     return context
--- cart/views.py
+++ cart/views.py
@@ -8,13 +8,13 @@ def view_cart(request):
     return render(request, 'cart/cart.html')
 
 def add_to_cart(request, item_id):
-     # View to add quantity to the shopping bag
+     # View to add quantity to the shopping cart
     
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
-    if itemd_id in list(bag.key()):
+    if item_id in list(cart.keys()):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
