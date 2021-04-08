494dc1403ec597752aed6e9d3ae0315619d95824
Alexander Forrest <asforrest@icloud.com>
2021-04-08 01:45:20 +0000

    Updating the shopping cart view

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your view
 
@@ -7,17 +7,29 @@ def view_cart(request):
     # View to return the contents of the shopping cart page
     return render(request, 'cart/cart.html')
 
+
 def add_to_cart(request, item_id):
-     # View to add quantity to the shopping cart
-    
+    # View to add quantity to the shopping cart
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
-
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
-    
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    # Adjust the quantity of subscriptions
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] += quantity
+    else:
+        cart.pop[item_id]
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
