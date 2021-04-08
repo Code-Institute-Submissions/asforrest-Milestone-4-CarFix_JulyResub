55aa5d6efff6862f726c79c5da05aede41200976
Alexander Forrest <asforrest@icloud.com>
2021-04-08 11:29:54 +0000

    Cart quantites are now working correctly

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
+from subscriptions.models import Subscription
 
 # Create your view
 
@@ -23,26 +26,41 @@ def add_to_cart(request, item_id):
 
 def adjust_cart(request, item_id):
     # Adjust the quantity of subscriptions
+    subscription = get_object_or_404(Subscription, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
+    print(quantity)
 
     if quantity > 0:
-        cart[item_id] += quantity
+        cart[item_id] = quantity
+        messages.success(request,
+                            (f'Updated {subscription.name} '
+                            f'quantity to {cart[item_id]}'))
     else:
-        cart.pop[item_id]
+        cart.pop(item_id)
+        messages.success(request,
+                            (f'Removed {subscription.name} '
+                            f'from your cart'))
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
 
 
+
+
+
 def remove_from_cart(request, item_id):
     try:
-        # Adjust the quantity of subscriptions
+        # Remove item from shopping cart
+        subscription = get_object_or_404(Subscription, pk=item_id)
         cart = request.session.get('cart', {})
-
-        cart.pop[item_id]
+        # ERROR MAY BE COMING FROM HERE, REMOVED SIZING INFO AND NEED TO ADD MESSAGING
+        cart.pop(item_id)
+        messages.success(request, f'Removed {subscription.name} from your cart')
 
         request.session['cart'] = cart
-        return HtttpResponse(status=200)
-    except Excetpion as e:
-        return HtttpResponse(status=500)
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
