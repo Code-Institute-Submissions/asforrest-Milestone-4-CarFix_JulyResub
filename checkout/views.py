c3f4248118a033eebf3fef1023bf6577db7d42e1
Alexander Forrest <asforrest@icloud.com>
2021-04-08 22:53:19 +0000

    Added Stripe JS and CSS

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IXHjKHjJQjzesItIzWaWTHYcsYDfOsvFsKLoGNqYr9jQTKpvD40zbIOKJydQLyQLSahXx50FjmsB8uRQh1S4dbM00bhoLcHvu',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
