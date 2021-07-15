6675aa0a60d689a9a26cc69c7f61ff9ec91fec19
Alexander Forrest <asforrest@icloud.com>
2021-07-15 22:45:08 +0000

    Fixed issue with Server 500 on Heroku, due to migration

--- carfix/settings.py
+++ carfix/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 ALLOWED_HOSTS = ['asforrest-carfix.herokuapp.com', 'localhost']
 
@@ -120,19 +120,19 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'carfix.wsgi.application'
 
-#if 'DATABASE_URL' in os.environ:
-#    DATABASES = {
-#        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#    }
-#else:
-#    DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.sqlite3',
-#            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#        }
-#    }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
-DATABASES = { 'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+
 
 
 # Password validation
